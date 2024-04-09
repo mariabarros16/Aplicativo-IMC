5f3ee23f7f866ef9bb6578bd11c85e1593ad6730
guarani2614 <maria.barros111@etec.sp.gov.br>
2024-04-09 12:10:55 -0300

    criar main e formulario

---
+++ SRC/Components/Form/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Text, TextInput} from "react-native";
+
+export default function Form(){
+    return (
+        <View>
+            <View>
+                <Text>Altura</Text>
+                <TextInput
+                placeholder="Ex.1.75"
+                KeyboardType="numeric"
+                />
+                <Text>Peso</Text>
+                <TextInput
+                placeholder="75.865"
+                keyboardType="numeric"
+                />
+                
+           </View>
+        </View>
+    );
+}
